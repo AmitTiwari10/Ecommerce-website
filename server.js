cf03c9b0254aa26a0973b0968e6f03be88592296
Amit Tiwari <at1484746@gmail.com>
2024-02-06 00:29:39 +0530

    structure of project is implemented

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
-const express = require("express");
-const color = require("colors");
+import express from "express";
+import color from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+//Configure env
+dotenv.config();
+
+//database config
+connectDB();
+
 //rest object
 const app = express();
 
+//middelwares
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+
 //rest api
 app.get("/", (req, res) => {
-  res.send({ message: "Welcome to ecommer app" });
+  res.send("<h1>Welcome to ecommerce app</h1>");
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`.bgCyan.white);
+  console.log(
+    `Server is running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
 });
