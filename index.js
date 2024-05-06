ccc1c2a7e4d88c071710290b24276df91c5642b6
Amit Tiwari <at1484746@gmail.com>
2024-05-07 00:26:51 +0530

    browers security feature updated

--- index.js
+++ index.js
@@ -23,7 +23,11 @@ connectDB();
 const app = express();
 
 //middelwares
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://symphonious-klepon-a8132c.netlify.app/",
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 // app.use(express.static(path.join(__dirname, "./client/build")));
